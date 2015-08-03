c2fde650412c32b3945f737268f13eda15158d93
Hanif Norman <hanif@comworth.com.sg>
2015-08-03 16:24:51 +0800

    Generated initial files

---
+++ myfitnesspal-foods/api/controllers/FoodController.js
@@ -0,0 +1,11 @@
+/**
+ * FoodController
+ *
+ * @description :: Server-side logic for managing foods
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
